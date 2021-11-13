e0cd0eed3e4a790705b8871986c463f6b9296f8f
nikolayivan <61462702+nikolayivan@users.noreply.github.com>
2021-11-13 17:28:06 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from multiapp import MultiApp
+from apps import DailyReport, PeriodReport
+
+from PIL import Image
+
+
+app = MultiApp()
+st.set_page_config(page_title = 'Лебёдки', layout = 'wide', page_icon='🏢')
+image = Image.open(r'C:\Users\testingcenter\Documents\LiftingApps\StreamlitApps\img\ВЦСПС.png')
+st.sidebar.image(image)
+
+# Add all your application here
+app.add_app("Ежедневный отчет", DailyReport.app)
+app.add_app("Отчет за период", PeriodReport.app)
+
+# The main app
+app.run()
